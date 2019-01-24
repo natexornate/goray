576f0c12b1edd8f64444fe3b55c589a973e56030
Nathaniel White <natexornate@gmail.com>
2019-01-24 17:26:14 +0000

    Step 1, output an image!

---
+++ goray.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+)
+
+type Vec3f struct {
+	X, Y, Z float32
+}
+
+func render() {
+	const width = 1024
+	const height = 768
+	im := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	outputFile, err := os.Create("out.png")
+	if err != nil {
+		fmt.Printf("Can't open output file")
+		return
+	}
+
+	framebuffer := [height * width]Vec3f{}
+
+	for j := 0; j < height; j++ {
+		for i := 0; i < width; i++ {
+			framebuffer[i+j*width] = Vec3f{float32(j) / float32(height), float32(i) / float32(width), 0}
+		}
+	}
+
+	for i, v := range framebuffer {
+		off := i * 4
+		im.Pix[off] = uint8(v.X * 255.)
+		im.Pix[off+1] = uint8(v.Y * 255.)
+		im.Pix[off+2] = uint8(v.Z * 255.)
+		im.Pix[off+3] = 255
+	}
+
+	png.Encode(outputFile, im)
+	outputFile.Close()
+}
+
+func main() {
+	fmt.Printf("Hello, World!")
+
+	render()
+}
