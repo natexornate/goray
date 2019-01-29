3f614803f105efa7c3e417c0fd606105e2695961
Nathaniel White <natexornate@gmail.com>
2019-01-29 18:34:59 +0000

    Add profiling options to binary

--- goray.go
+++ goray.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+	"runtime/trace"
 
 	"github.com/natexornate/goray/goray"
 )
 
-func main() {
-	fmt.Printf("Rendering Scene!\n")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+var tracefile = flag.String("tracefile", "", "write trace profile to file")
 
+func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	if *tracefile != "" {
+		f, err := os.Create(*tracefile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		trace.Start(f)
+		defer trace.Stop()
+	}
+
+	fmt.Printf("Rendering Scene!\n")
 	goray.Scene()
+
+	return
 }
